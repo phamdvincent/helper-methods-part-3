bc1ef307026efbe0d77959060240f68fca4ca46a
Vincent Pham <40014512+phamdvincent@users.noreply.github.com>
2024-02-20 17:40:32 +0000

    Added released on column and forms

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -54,7 +54,7 @@ class MoviesController < ApplicationController
   private
 
   def movie_params
-    params.require(:movie).permit(:title, :description, :image_url)
+    params.require(:movie).permit(:title, :description, :image_url, :released_on)
   end
 
 end
